ecc33d1e7bb82743169606387da12845fca23ba7
Martin Horshovskyy <martyn.horshovskyy@gmail.com>
2019-09-23 00:28:00 +0300

    Add project files.

---
+++ 2/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace _2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string text;
+        private string directory;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BrowseButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Create OpenFileDialog
+            Microsoft.Win32.OpenFileDialog openFileDlg = new Microsoft.Win32.OpenFileDialog();
+
+            // Launch OpenFileDialog by calling ShowDialog method
+            Nullable<bool> result = openFileDlg.ShowDialog();
+            // Get the selected file name and display in a TextBox.
+            // Load content of file in a TextBlock
+
+            if (result == true)
+            {
+                Filename.Text = openFileDlg.FileName;
+                directory = System.IO.Path.GetDirectoryName(openFileDlg.FileName);
+
+
+                text = File.ReadAllText(openFileDlg.FileName, Encoding.Default);
+            }
+        }
+
+        private void CodeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(A.Text, out int a) && int.TryParse(B.Text, out int b))
+            {
+                using (var file = new StreamWriter($"{directory}/coded.txt"))
+                {
+                    var code = new Code(a, b);
+                    var codedText = code.Encrypt(text);
+                    file.WriteLine(codedText);
+                }
+            }
+            else
+            {
+                isValid.Text = "a and b are not valid";
+            }
+
+        }
+    }
+}
---
+++ 2/Сode.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    class Code
+    {
+        private static readonly List<char> UA_Alphabet = new List<char>
+        {
+            'а', 'б', 'в', 'г', 'ґ', 'д', 'е', 'є', 'ж', 'з', 'и',
+            'і', 'ї', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с',
+            'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ь', 'ю', 'я'
+        };
+        private Dictionary<char, char> Table { get; } = new Dictionary<char, char>();
+        public int A { get; set; }
+        public int B { get; set; }
+
+        public Code(int a, int b)
+        {
+            A = a;
+            B = b;
+            for (int i = 0; i < UA_Alphabet.Count; i++)
+            {
+                int index = (A * i + B) % UA_Alphabet.Count;
+                Table.Add(UA_Alphabet[i], UA_Alphabet[index]);
+            }
+        }
+
+        public string Encrypt(string text)
+        {
+            string res = "";
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (Table.Keys.Contains(text[i]))
+                {
+                    res += Table[text[i]];
+                }
+                else
+                {
+                    res += text[i];
+                }
+            }
+            return res;
+        }
+    }
+}
